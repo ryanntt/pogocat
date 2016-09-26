6eea3c53dd877c6cd7d30b901567d8938c514899
Ryan Nguyen <trungthng@gmail.com>
2016-09-27 00:02:37 +0800

    v0 working

---
+++ js/app.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+  //Create a list of pokemon
+  var pokemons =[];
+  var dustLevels = [];
+  var list = godex.pokemon;
+  var dustList = dex.dustLevels;
+
+  for (p in list) {
+    var pokemon = list[p];
+    pokemons.push({id: pokemon['id'], name: pokemon['name']});
+  }
+
+  for (d in dustList) {
+    dustLevels.push({stardust: dustList[d]});
+    // console.log(dust);
+  }
+
+  //Populate the select field with Pokemon List
+  $('#pokemonName').selectize({
+    maxItems: 1,
+    valueField: 'name',
+    labelField: 'name',
+    searchField: 'name',
+    options: pokemons,
+    create: false
+  });
+
+  $('#stardust').selectize({
+    maxItems: 1,
+    valueField: 'stardust',
+    labelField: 'stardust',
+    searchField: 'stardust',
+    options: dustLevels,
+    create: false
+  });
+});
+
+function checkIV() {
+  if ( isFormFilled() ) {
+    var result = dex.appraise({
+      pokemon: $('#pokemonName').val().toString(),
+      cp: $('#cp').val(),
+      hp: $('#hp').val(),
+      dust: $('#stardust').val(),
+      powered: $('#powered-up').prop('checked'),    // powered up?
+      strongAtk: $('#attack').prop('checked'), // Gym Leader says "x" is
+      strongDef: $('#defense').prop('checked'),  // your poke's best stat,
+      strongHP: $('#stamina').prop('checked')   // send that as true.
+    });
+    var grade = result["grade"];
+    $('.cp-min').text(grade["min"]);
+    $('.cp-max').text(grade["max"]);
+  }
+
+};
+
+function changePokemon(event) {
+  if ( event.target.value != "" ) {
+    var pokemonDetails = dex.get(event.target.value);
+    $('.cp-max-overall').text(pokemonDetails['maxCP']);
+    $('.type-overall').text(function() {
+      var types = "";
+      for ( var i=0; i<pokemonDetails['type'].length; i++) {
+        types += pokemonDetails['type'][i].replace(/\b\w/g, l => l.toUpperCase()) + " ";
+      }
+      return types;
+    });  
+  }
+};
+
+function isFormFilled() {
+  return ( ($('#pokemonName').val().toString() != "") && 
+    ( $('#cp').val() != "") && 
+    ( $('#hp').val() != "") &&
+    ( $('#stardust').val() != "")
+  );
+};
